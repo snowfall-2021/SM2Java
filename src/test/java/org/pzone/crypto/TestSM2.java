670c720a3871d8749e3163cfea3374f614c24725
ult <afgugewufv@gmail.com>
2021-05-12 16:23:01 +0800

    bbb

--- src/test/java/org/pzone/crypto/TestSM2.java
+++ src/test/java/org/pzone/crypto/TestSM2.java
@@ -3,23 +3,22 @@ package org.pzone.crypto;
 import org.bouncycastle.math.ec.ECPoint;
 
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class TestSM2 {
 
     public static void main(String[] args) {
         SM2 x = new SM2();
+        Scanner scan= new Scanner(System.in);
         SM2KeyPair keys = x.generateKeyPair();
         ECPoint publicKey = keys.getPublicKey();
         BigInteger privateKey = keys.getPrivateKey();
-        Scanner scan= new Scanner(System.in);
         System.out.println("Public key: " + publicKey.toString());
         System.out.println("Private key: " + privateKey.toString());
         System.out.print("Enter your Cleartext: ");
         byte[] data = x.encrypt(scan.nextLine(), publicKey);
         System.out.print("Ciphertext: ");
-        System.out.println(new String(data, StandardCharsets.UTF_8));
+        System.out.println(x.bytesToHex(data));
         String origin = x.decrypt(data, privateKey);
         System.out.println("Decipher: " + origin);
     }
